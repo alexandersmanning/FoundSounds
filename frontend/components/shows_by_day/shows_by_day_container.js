4c2ae77103cfd864dd3a4af3d926a32f1bc5eb82
alexandersmanning <alexandersmanning@gmail.com>
2016-12-14 19:46:08 -0800

    finished cleaning up dates with proper date utils

--- frontend/components/shows_by_day/shows_by_day_container.js
+++ frontend/components/shows_by_day/shows_by_day_container.js
@@ -1,26 +1,31 @@
-import { connect } from 'react-redux'
-import ShowsByDay from './shows_by_day'
-import { fetchShowsByDate } from '../../actions/shows_by_day_actions'
-import { updateDates, removeVenueFromFilter } from '../../actions/filter_actions'
-import { addUserShow, updateUserShow, deleteUserShow } from '../../actions/user_shows_actions'
+import { connect } from 'react-redux';
+import ShowsByDay from './shows_by_day';
+import { fetchShowsByDate } from '../../actions/shows_by_day_actions';
+
+import { updateDates, 
+	removeVenueFromFilter } from '../../actions/filter_actions';
+
+import { addUserShow, 
+	updateUserShow, 
+	deleteUserShow } from '../../actions/user_shows_actions';
+
+import { getDefaultToDate,
+	getDefaultFromDate,
+	dateToString
+} from '../../util/date_util';
 
 const mapStateToProps = (state, ownProps) => {
-	let today = new Date().toISOString().substring(0, 10);
-	let fromDate, toDate;
-	if (!ownProps.location.query.fromDate || Date.parse(ownProps.location.query.fromDate) < Date.parse(today) ) {
-		fromDate = today
-		toDate = new Date(+new Date + 6048e5).toISOString().substring(0, 10)
-	} else {
-		fromDate = Date.parse(ownProps.location.query.fromDate)
-		toDate = Date.parse(ownProps.location.query.toDate)
-		if (!fromDate) {
-			fromDate = new Date().toISOString().substring(0, 10)
-		} else { fromDate = new Date(fromDate).toISOString().substring(0, 10) } 
-	
-		if (!toDate) {
-			toDate = new Date().toISOString().substring(0, 10)
-		} else { toDate = new Date(toDate).toISOString().substring(0, 10) } 
-	}
+	let today = getDefaultFromDate();
+	let fromDate = ownProps.location.query.fromDate;
+	let toDate = ownProps.location.query.toDate;
+
+	if (!fromDate || Date.parse(fromDate) < Date.parse(today) ) {
+		fromDate = today;
+	} 
+
+	if (!toDate || Date.parse(toDate) < Date.parse(fromDate)){
+		toDate = fromDate; 
+	} 
 
 	return ({
 		ShowsByDay: state.ShowsByDay,
